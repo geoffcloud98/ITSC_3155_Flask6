c0519f02d05161519a2ddb2ce779cee1f7b713f5
Geoffrey <gcloud@uncc.edu>
2022-12-07 13:47:48 -0500

    flask 6 - fully functional

--- Flask06.py
+++ Flask06.py
@@ -9,8 +9,8 @@ from flask import redirect, url_for
 from DATABASE import db
 from MODELS import Note as Note
 from MODELS import User as User
-from FORMS import RegisterForm
-from FORMS import LoginForm
+from MODELS import Comment as Comment
+from FORMS import RegisterForm, LoginForm, CommentForm
 from flask import session
 import bcrypt
 
@@ -50,13 +50,16 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    # retrive user from data base
-    a_user = db.session.query(User).filter_by(email='gcloud@uncc.edu').one()
-    # retrieve note from database
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    # check if a user is saved in session
+    if session.get('user'):
+        # retrieve note from database
+        my_note = db.session.query(Note).filter_by(id=note_id, user_id=session['user_id']).one()
 
-    
-    return render_template('note.html', note = my_note, user = a_user)
+        # create a comment form object
+        form = CommentForm()
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
@@ -183,6 +186,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
