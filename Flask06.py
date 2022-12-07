e72ed63714d6e967f9702dfe1de02cce7e68ef5f
Geoffrey <gcloud@uncc.edu>
2022-12-07 11:25:43 -0500

    flask at stage #4 - updated queries to return one

--- Flask06.py
+++ Flask06.py
@@ -2,16 +2,21 @@
 
 # imports
 import os                 # os is used to get environment variables IP & PORT
+import os.path
+import sqlite3
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for 
 from DATABASE import db
+from MODELS import Note as Note
+from MODELS import User as User
+
 
 
 app = Flask(__name__)     # create an app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_appp.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 #  Bind SQLAlchemy db object to this Flask app
@@ -20,10 +25,8 @@ db.init_app(app)
 # Setup models
 with app.app_context():
     db.create_all()   # run under the app context
-    
-notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 
-             2: {'title':'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title':'Third note', 'text': 'This is my Third note', 'date': '10-3-2020'}}
+
+
 
 
 # @app.route is a decorator. It gives the function "index" special powers.
@@ -32,27 +35,30 @@ notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Geoffrey', 'email':'gcloud@uncc.edu'}
+    # retrieve user from database
+    a_user = db.session.query(User).filter_by(email='gcloud@uncc.edu').one()
 
     return render_template("index.html", user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = {'name': 'Geoffrey', 'email':'gcloud@uncc.edu'}
+    # retrieve user from database
+    a_user = db.session.query(User).filter_by(email='gcloud@uncc.edu').one()
+    # retrieve notes from database
+    my_notes = db.session.query(Note).all()
 
-    return render_template('notes.html', notes = notes, user = a_user)
+    return render_template('notes.html', notes = my_notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = {'name': 'Geoffrey', 'email':'gcloud@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='gcloud@uncc.edu').one()
+    my_note = db.session.query(Note).filter_by(id=note_id)
 
     
-    return render_template('note.html', note = notes[int(note_id)], user = a_user)
+    return render_template('note.html', note = my_note[int(note_id)], user = a_user)
 
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
-    # mock user
-    a_user = {'name': 'Geoffrey', 'email': 'gcloud@gmail.com'}
 
     #check if method is used for request
     if (request.method == 'POST'):
@@ -62,16 +68,19 @@ def new_note():
         text = request.form['noteText']
         #create date stamp
         from datetime import date
-        today = date.today();
+        today = date.today()
         #format mm//dd//yyyy
         today = today.strftime("%m-%d-%Y")
-        #get the last ID used and incremement by 1
-        id = len(notes)+1
-        #create new note entry
-        notes[id] = {'title': title, 'text': text, 'date': today}
+        new_record = Note(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
+
         #ready to render response - redirect to notes listing
         return redirect(url_for('get_notes'))
     else:
+        # GET request - show new note form
+        # retrieve user from database
+        a_user = db.session.query(User).filter_by(email='gcloud@uncc.edu').one()
         return render_template('new.html', user = a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
